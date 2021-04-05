8af5b6bec566fb4af07e011344acbdbcdcba6018
Meg0001 <wrbjp627@yahoo.co.jp>
2021-04-05 10:45:55 +0900

    authDemoデータベースをmongoDBに作成

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); 
+
+mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!");
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!");
+        console.log(err);
+    });
 
 app.set('view engine' ,'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({extended: true})); //we are pursing the request body
+
+app.get('/', (req, res) => {
+    res.send("THIS IS THE HOME PAGE")
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async(req, res) => {
+    const {password, username} = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/')
 })
 
 app.get('/secret', (req, res) => {

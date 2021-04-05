03207c93a463d6adab015863fa6c37e5e891cc36
Meg0001 <wrbjp627@yahoo.co.jp>
2021-04-05 13:46:20 +0900

    add login route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcrypt');
 
 mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -14,10 +14,10 @@ mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, 
         console.log(err);
     });
 
-app.set('view engine' ,'ejs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.urlencoded({extended: true})); //we are pursing the request body
+app.use(express.urlencoded({ extended: true })); //we are pursing the request body
 
 app.get('/', (req, res) => {
     res.send("THIS IS THE HOME PAGE")
@@ -27,8 +27,8 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
-app.post('/register', async(req, res) => {
-    const {password, username} = req.body;
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
     const hash = await bcrypt.hash(password, 12);
     const user = new User({
         username,
@@ -36,6 +36,21 @@ app.post('/register', async(req, res) => {
     })
     await user.save();
     res.redirect('/')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+app.post('/login', async(req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const validPassword = await bcrypt.compare(password, user.password)
+    if(validPassword){
+        res.send("YAY WELCOME!")
+    } 
+    else{
+        res.send("TRY AGAIN")
+    }
 })
 
 app.get('/secret', (req, res) => {
